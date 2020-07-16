0454d32ddd64277729cb106d067273ef55ef2a7c
princedavitt <64537102+princedavitt@users.noreply.github.com>
2020-07-17 00:36:57 +0400

    Update translation.py

--- translation.py
+++ translation.py
@@ -15,7 +15,7 @@ This is a Telegram <b>Name It</> Bot!
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
     NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
-    SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail saved. This image will be used in the File."
+    SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail saved. This image will be used in the Future Uploads."
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
@@ -28,9 +28,9 @@ This is a Telegram <b>Name It</> Bot!
 2. Reply to that message to /rename new name.extension.
 
 --------
-
-Support Group : @VR_Labs
-© @SonaSupport"""
+Join Group before sending File ,Otherwise your file will be currupted
+Support Group : @CGlitz
+© @PrinceDavitt"""
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.."
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
     FREE_USER_LIMIT_Q_SZE = """Cannot Process.
