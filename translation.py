f5113bf558557edc21a2febc56102f38d5008b6d
princedavitt <64537102+princedavitt@users.noreply.github.com>
2020-07-17 00:03:25 +0400

    Update translation.py

--- translation.py
+++ translation.py
@@ -8,10 +8,10 @@ This is a Telegram <b>Name It</> Bot!
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
-    DOWNLOAD_START = "trying to download"
-    UPLOAD_START = "trying to upload"
+    DOWNLOAD_START = "File downloading"
+    UPLOAD_START = "File uploading.Dont Forgot to join @cgliz"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me🤓.**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using @Cglitz🤓.**"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
     NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
@@ -28,7 +28,7 @@ This is a Telegram <b>Name It</> Bot!
 2. Reply to that message to /rename new name.extension.
 
 --------
-
+Join @cglitz group otherwise u will get currupted File
 Support Group : @Cglitz
 © @Princedavitt"""
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.."
